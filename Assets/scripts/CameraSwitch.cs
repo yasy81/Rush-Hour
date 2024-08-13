e13d71450babbbe9f1b0f5cdd7c0310d0a3a9b56
yasy81 <77388658+yasy81@users.noreply.github.com>
2024-08-14 00:31:29 +0330

    cameraswitch is working properly in level one yay

--- Assets/scripts/CameraSwitch.cs
+++ Assets/scripts/CameraSwitch.cs
@@ -6,19 +6,24 @@ public class CameraSwitch : MonoBehaviour
 {
     public GameObject cam1; 
     public GameObject cam2;
-    // Update is called once per frame
+    
     void Update()
     {
-        if(Input.GetButtonDown("key1"))
+        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             cam1.SetActive(true);
-            cam1.SetActive(false);
-        }
+            cam2.SetActive(false);
+            cam1.tag = "MainCamera";
+            cam2.tag = "Untagged";
+        } 
 
-        if(Input.GetButtonDown("key2"))
+        if(Input.GetKeyDown(KeyCode.Alpha2))
         {
             cam1.SetActive(false);
-            cam1.SetActive(true);
+            cam2.SetActive(true);
+            cam1.tag = "Untagged";
+            cam2.tag = "MainCamera";
         }
     }
 }
