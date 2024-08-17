22e5a58aa55db764ab099201ea37bed2239777ac
yasy81 <77388658+yasy81@users.noreply.github.com>
2024-08-17 12:40:53 +0330

    two butterflyconfettis that go off once the player passes the finish line and then the next scene loads

--- Assets/scripts/FinishLine.cs
+++ Assets/scripts/FinishLine.cs
@@ -1,19 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    
+    public GameObject ButterflyConfetti;
+    public GameObject ButterflyConfetti1;
+
     private void OnTriggerEnter(Collider collision)
     {
-        
-
         if (collision.tag == "Car")
         {
-        
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            ButterflyConfetti.SetActive(true); 
+            ButterflyConfetti1.SetActive(true);// Activate the ButterflyConfetti
+            StartCoroutine(WaitAndLoadNextScene()); // Start the coroutine to wait and then load the next scene
         }
+    }
+
+    private IEnumerator WaitAndLoadNextScene()
+    {
+        // Wait until ButterflyConfetti is active
+        while (!ButterflyConfetti.activeInHierarchy)
+        {
+            yield return null; // Wait for the next frame
+        }
+
+        // Wait for an additional 3 seconds
+        yield return new WaitForSeconds(3f);
+
+        // Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
